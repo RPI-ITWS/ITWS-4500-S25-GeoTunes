f5ba3ec4aa4e571d3c62f6e1a317f2b6dfbe7025
Istar33 <lee.chris.h.isaac@gmail.com>
2025-02-14 13:26:08 -0500

    Initialize Node
    Initialize node backend

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://leei8:2sja71D1GTEUprrA@cluster0.2path.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  console.log("running");
+  try {
+    console.log("connecting");
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+    console.log("ending");
+  }
+}
+run().catch(console.dir);
+
+app.use(express.static('public'))
+
+app.get('/locale/:locale', (req, res) => {
+})
+
+app.post('/locale/:locale', (req, res) => {
+})
+
+app.put('/locale/:locale', (req, res) => {
+})
+
+app.delete('/locale/:locale', (req, res) => {
+})
+
+app.listen(port, () => {
+  console.log('Listening on *:3000')
+})
+
