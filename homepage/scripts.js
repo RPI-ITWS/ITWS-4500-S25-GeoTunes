ef754026a4a0660e632a84e000bdcb080c14f8ca
Izik Bakhshiyev <138522345+izikb56@users.noreply.github.com>
2025-02-18 13:06:53 -0500

    homepage

---
+++ homepage/scripts.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function Navbar() {
+  return React.createElement(
+    'nav',
+    { className: 'navbar' },
+    React.createElement('div', { className: 'logo' }, 'LOGO'),
+    React.createElement(
+      'a',
+      { className: 'login-link', href: '#login' },
+      'LOGIN/SIGN UP'
+    )
+  );
+}
+
+function MainContent() {
+  return React.createElement(
+    'div',
+    { className: 'main-content' },
+    React.createElement('h1', {}, 'GeoTunes'),
+    React.createElement('p', {}, 'Mini desc describing concept'),
+    React.createElement(
+      'div',
+      { className: 'button-container' },
+      React.createElement(
+        'a',
+        { className: 'btn', href: '#signup' },
+        'Sign Up'
+      ),
+      React.createElement(
+        'a',
+        { className: 'btn', href: '#login' },
+        'Login'
+      )
+    )
+  );
+}
+
+const rootNode = document.getElementById('root');
+const root = ReactDOM.createRoot(rootNode);
+root.render(
+  React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(Navbar),
+    React.createElement(MainContent)
+  )
+);
